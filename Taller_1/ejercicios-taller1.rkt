e7633bc41795d2a69d6605d13636d1be7a402e7a
yenreh <qherney@gmail.com>
2023-03-21 22:20:21 -0500

    Ejercicio 1

--- Taller_1/ejercicios-taller1.rkt
+++ Taller_1/ejercicios-taller1.rkt
@@ -9,3 +9,17 @@
 
 ;Ejercicio 1
 
+(define invert
+  (lambda (l p)
+    [cond
+      [(null? l) empty]
+      [(and (p (caar l)) (p (cadar l))) (cons (car l) (invert (cdr l) p))]
+      [else (invert (cdr l) p)]
+      ]
+    )
+  )
+
+;Pruebas
+(invert '((3 2) (4 2) (1 5) (2 8)) even?)
+(invert '((6 9) (10 90) (82 7) ) odd? )
+
